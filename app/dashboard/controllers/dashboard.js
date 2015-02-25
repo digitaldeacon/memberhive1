d0bbd78197f0dade065e986a774ff2c6ceb23371
Samuel Mehrbrodt <s.mehrbrodt@gmail.com>
2015-02-25 11:41:26 +0100

    Fix dashboard user count display

--- app/dashboard/controllers/dashboard.js
+++ app/dashboard/controllers/dashboard.js
@@ -1,5 +1,5 @@
 angular.module('gemmiiWebApp')
-  .controller('DashboardCtrl', ($scope, Person) => {
+  .controller('DashboardCtrl', function($scope, Person) {
      $scope.userCount = Person.count(count => {
        $scope.userCount = count.count;
     });
