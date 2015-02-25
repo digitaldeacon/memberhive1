1a632f91cc6900b6234f0327ee279e0d460efbc9
EBTC <th@ebtc-online.org>
2015-02-25 14:22:18 +0100

    Merge branch 'master' of github.com:ebtc/gemmii-web
    # Conflicts:
    #	app/dashboard/views/dashboard.html

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,12 +11,20 @@ var GemmiiApp = angular.module('gemmiiWebApp', [
   'picardy.fontawesome',
   'ui.router',
   'ui.bootstrap',
+  'oc.lazyLoad',
   'lbServices',
 
   'gem.person',
   'gem.dashboard',
   'gem.auth'
 ]);
+
+GemmiiApp.config(['$ocLazyLoadProvider', function($ocLazyLoadProvider) {
+  $ocLazyLoadProvider.config({
+    cssFilesInsertBefore: 'ng_load_plugins_before' // load the above css files before a LINK element with this ID. Dynamic CSS files must be loaded between core and theme css files
+  });
+}]);
+
 
 GemmiiApp.factory('settings', ['$rootScope', $rootScope => {
   // supported languages
