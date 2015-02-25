3a01eb2fae7b183753eef0b1b7392bad045650d9
EBTC <th@ebtc-online.org>
2015-02-25 18:59:57 +0100

    Merge branch 'master' of github.com:ebtc/gemmii-web
    # Conflicts:
    #	app/dashboard/views/dashboard.html

--- app/dashboard/controllers/dashboard.js
+++ app/dashboard/controllers/dashboard.js
@@ -1,6 +1,9 @@
 angular.module('gem.dashboard')
-  .controller('DashboardController', function($scope, Person) {
+  .controller('DashboardController', function($scope, Person, $timeout) {
     Person.count(count => {
        this.userCount = count.count;
+    });
+    $scope.$on('$viewContentLoaded', () => {
+      PortletDraggable.init();
     });
   });
