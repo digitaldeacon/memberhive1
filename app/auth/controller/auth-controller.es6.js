b0430ad6ff6cb698d4500b77583457cf0983976f
Paul Walger <metaxy@walger.name>
2015-03-04 09:03:23 +0100

    more userfriendly login

--- app/auth/controller/auth-controller.es6.js
+++ app/auth/controller/auth-controller.es6.js
@@ -1,17 +1,27 @@
-function LoginController(User) {
+function LoginController(User, $location) {
   function login() {
     User.login(
-        {rememberMe: this.rememberMe},
-        {username: this.username, password: this.password},
-        (err, accessToken) => {
-          console.log(accessToken);
-        }
+      {rememberMe: this.rememberMe},
+      {username: this.username, password: this.password},
+      (val, accessToken) => {
+        this.error = false;
+        $location.path("/dhasboard");
+      },
+      (err) => {
+        this.error = true;
+        this.error_msg = err.data.error.name;
+        this.error_code = err.data.error.code;
+      }
     );
   }
   this.rememberMe = true;
   this.username = '';
   this.password = '';
   this.login = login;
+  
+  this.error = false;
+  this.error_msg = "";
+  this.error_code = "";
 }
 
 angular.module('gem.auth').controller('LoginController', LoginController);
