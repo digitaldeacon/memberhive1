b3efe10baee02d3071531d6663eaa1fd2ed1eea7
Paul Walger <metaxy@walger.name>
2015-03-05 20:54:21 +0100

    a basic acl for reports

--- common/models/report.js
+++ common/models/report.js
@@ -1,3 +1,18 @@
+var loopback = require('loopback');
 module.exports = function(Report) {
-
+  
+  Report.beforeCreate = function(next, modelInstance) {
+    modelInstance.ownerId = loopback.getCurrentContext().get('accessToken').userId;
+    next();
+  };
+  
+  //return only the results, which belong to the user
+  Report.observe('access', function limitToUser(ctx, next) {
+    if(ctx.query.where === undefined)
+      ctx.query.where = {};
+    ctx.query.where.ownerId = loopback.getCurrentContext().get('accessToken').userId;
+    
+    next();
+  });
 };
+
