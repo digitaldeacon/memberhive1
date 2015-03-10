88691b177516a530b8901f7ec53ffa9c07810bdc
Samuel Mehrbrodt <s.mehrbrodt@gmail.com>
2015-03-10 16:59:16 +0100

    Show nickname in brackets

--- app/person/filters/person-filters.es6.js
+++ app/person/filters/person-filters.es6.js
@@ -12,14 +12,14 @@ angular.module('personFilters', [])
   .filter('formatName', function() {
     return function(person) {
       if (person.nickName)
-        return `${person.nickName} ${person.lastName}`;
+        return `${person.firstName} ${person.middleName} ${person.lastName} (${person.nickName})`;
       return `${person.firstName} ${person.middleName} ${person.lastName}`;
     };
   })
   .filter('formatFirstName', function() {
     return function(person) {
       if (person.nickName)
-        return person.nickName;
+        return `${person.firstName} ${person.middleName} (${person.nickName})`;
       return `${person.firstName} ${person.middleName}`;
     };
   });
