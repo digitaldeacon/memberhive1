ef312529175382fbc4a0c6a6fb6522adf301400f
Paul Walger <metaxy@walger.name>
2015-03-11 11:25:40 +0100

    fix some errors

--- common/models/text.js
+++ common/models/text.js
@@ -1,14 +1,15 @@
 var loopback = require('loopback');
 module.exports = function(Text) {
-  var userId = loopback.getCurrentContext().get('accessToken').userId;
 
   Text.beforeCreate = function(next, modelInstance) {
+    var userId = loopback.getCurrentContext().get('accessToken').userId;
     modelInstance.ownerId = userId;
     next();
   };
 
   //return only the results, which belong to the user
   Text.observe('access', function limitToUser(ctx, next) {
+    var userId = loopback.getCurrentContext().get('accessToken').userId;
     if(ctx.query.where === undefined) {
       ctx.query.where = {};
     }
