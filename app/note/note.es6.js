d8b58010c98fd50a8806b78f439cbea0875037d8
Paul Walger <metaxy@walger.name>
2015-03-11 11:54:28 +0100

    rename to note

---
+++ app/note/note.es6.js
@@ -0,0 +1,31 @@
+angular.module('gem.note').config(
+  ($stateProvider, $compileProvider) => {
+    $stateProvider.state('note', {
+      url: '/note',
+      template: '<ui-view/>',
+      abstract: true,
+      data: {
+        module: 'person',
+        pageTitle: 'Person',
+      }
+    }).state('note.list', {
+      url: '/list',
+      templateUrl: '../note/views/note.list.html',
+      data: {
+        pageSubTitle: 'Create and edit persons'
+      },
+      acl: {
+        needRights: ['$authenticated']
+      }
+    }).state('note.create', {
+      url: '/create',
+      templateUrl: '../note/views/note.create.html',
+      data: {
+        pageSubTitle: 'Create a person'
+      },
+      acl: {
+        needRights: ['$authenticated']
+      }
+    });
+  }
+);
--- common/models/note.js
+++ common/models/note.js
@@ -1,14 +1,14 @@
 var loopback = require('loopback');
-module.exports = function(Text) {
+module.exports = function(Note) {
 
-  Text.beforeCreate = function(next, modelInstance) {
+  Note.beforeCreate = function(next, modelInstance) {
     var userId = loopback.getCurrentContext().get('accessToken').userId;
     modelInstance.ownerId = userId;
     next();
   };
 
   //return only the results, which belong to the user
-  Text.observe('access', function limitToUser(ctx, next) {
+  Note.observe('access', function limitToUser(ctx, next) {
     var userId = loopback.getCurrentContext().get('accessToken').userId;
     if(ctx.query.where === undefined) {
       ctx.query.where = {};
