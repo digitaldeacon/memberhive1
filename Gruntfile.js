06f92a642341696f733e2079eb15b9d6c3904831
Samuel Mehrbrodt <s.mehrbrodt@gmail.com>
2015-03-12 16:35:12 +0100

    Watch js files for changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,7 +93,7 @@ module.exports = function(grunt) {
         files: [
           '<%= yeoman.app %>/**/*.html',
           '.tmp/styles/{,*/}*.css',
-          '.tmp/scripts/{,*/}*.js',
+          '.tmp/scripts/**/*.js',
           '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
         ]
       }
