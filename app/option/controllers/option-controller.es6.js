2805a41ed5965ed40676dbe63baec944ad62af69
Samuel Mehrbrodt <s.mehrbrodt@gmail.com>
2015-03-27 09:58:26 +0100

    Cleanup OptionController and View

--- app/option/controllers/option-controller.es6.js
+++ app/option/controllers/option-controller.es6.js
@@ -1,60 +1,52 @@
 export function OptionController(Option) {
-  var main = this;
-  function getOptions() {
-    Option.find(result => main.options = result);
-  }
-
-  function createOption(option) {
-    Option.create(option, () => {
-      initCreateForm();
-      getOptions();
-    });
-  }
-
-  function updateOption(optionObj) {
-    Option.upsert(optionObj, () => {
-      cancelEditing();
-      getOptions();
-    });
-  }
-
-  function deleteOption(optionId) {
-    Option.deleteById({id: optionId}, () => {
-      cancelEditing();
-      getOptions();
-    });
-  }
-
-  function initCreateForm() {
-    main.newOption = {optionName: '', optionValue: ''};
-  }
-
-  function setEditedOption(option) {
-    main.editedOption = angular.copy(option);
-    main.isEditing = true;
-  }
-
-  function isCurrentOption(optionId) {
-    return main.editedOption !== null && main.editedOption.id === optionId;
-  }
-
-  function cancelEditing() {
-    main.editedOption = null;
-    main.isEditing = false;
-  }
-
   this.options = [];
   this.editedOption = null;
   this.newOption = null;
   this.isEditing = false;
-  this.getOptions = getOptions;
-  this.createOption = createOption;
-  this.updatePerson = updateOption;
-  this.deletePerson = deleteOption;
-  this.setEditedOption = setEditedOption;
-  this.isCurrentOption = isCurrentOption;
-  this.cancelEditing = cancelEditing;
 
-  initCreateForm();
-  getOptions();
+  this.getOptions = () => {
+    Option.find(result => this.options = result);
+  };
+
+  this.createOption = (option) => {
+    Option.create(option, () => {
+      this.initCreateForm();
+      this.getOptions();
+    });
+  };
+
+  this.updateOption = (optionObj) => {
+    Option.upsert(optionObj, () => {
+      this.cancelEditing();
+      this.getOptions();
+    });
+  };
+
+  this.deleteOption = (optionId) => {
+    Option.deleteById({id: optionId}, () => {
+      this.cancelEditing();
+      this.getOptions();
+    });
+  };
+
+  this.initCreateForm = () => {
+    this.newOption = {optionName: '', optionValue: ''};
+  };
+
+  this.setEditedOption = (option) => {
+    this.editedOption = angular.copy(option);
+    this.isEditing = true;
+  };
+
+  this.isCurrentOption = (optionId) => {
+    return this.editedOption !== null && this.editedOption.id === optionId;
+  };
+
+  this.cancelEditing = () => {
+    this.editedOption = null;
+    this.isEditing = false;
+  };
+
+  this.initCreateForm();
+  this.getOptions();
 }
