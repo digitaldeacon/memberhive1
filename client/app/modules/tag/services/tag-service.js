c3e94e09051c0e217bd3a73cfdec62b9177f896f
EBTC <th@ebtc-online.org>
2015-04-16 20:29:05 +0200

    cleanups and icons

--- client/app/modules/tag/services/tag-service.js
+++ client/app/modules/tag/services/tag-service.js
@@ -22,26 +22,6 @@ export function TagService(Tag, TagEntity, Shout, gettextCatalog) {
     },
     save: (tagsObj, entityId, rowId) => {
       Tag.saveTagsEntity({"tags": tagsObj, "entityId": entityId, "rowId": rowId});
-      /*tagsObj.forEach( tag => {
-        tag.siteId = 1;
-        console.log(tag);
-        Tag.upsert({}, tag).$promise.then(
-          (data) => {
-            //Shout.success(gettextCatalog.getString('Successfully saved tag “{{name}}”', {name: data.text}));
-            var entityObj = {
-              tagId: data.id,
-              rowId: rowId,
-              entityId: entityId
-            };
-            TagEntity.upsert({},entityObj).$promise.then(data=>{
-              console.log('Success');
-            });
-          },
-          (error) => {
-            Shout.error(error.data.error.message, error.data.error.name);
-          }
-        );
-      });*/
     }
   };
 }
