d1b08527e92d02d27800fc7c154d2183fd647184
Paul Walger <metaxy@walger.name>
2015-04-23 13:39:47 +0200

    working on note

--- common/models/note.js
+++ common/models/note.js
@@ -1,24 +1,25 @@
-var loopback = require('loopback');
 module.exports = function(Note) {
   Note.definition.rawProperties.created.default =
   Note.definition.properties.created.default = function() {
         return new Date();
   };
 
-  Note.beforeCreate = function(next, modelInstance) {
-    var userId = loopback.getCurrentContext().get('accessToken').userId;
-    modelInstance.ownerId = userId;
+  Note.observe('before save', function(ctx, next) {
+    console.log("note - before save called");
+    var userId = Note.app.loopback.getCurrentContext().get('accessToken').userId;
+    ctx.instance.ownerId = userId;
     next();
-  };
+  });
 
   //return only the results, which belong to the user
   Note.observe('access', function limitToUser(ctx, next) {
-    var userId = loopback.getCurrentContext().get('accessToken').userId;
+    console.log("note - access");
+    var userId = Note.app.loopback.getCurrentContext().get('accessToken').userId;
     if(ctx.query.where === undefined) {
       ctx.query.where = {};
     }
+    console.log(userId);
     ctx.query.where.ownerId = userId;
-
     next();
   });
 };
