ce38748fdf16d5cb972508bbac4ef8332d644f08
Samuel Mehrbrodt <s.mehrbrodt@gmail.com>
2015-04-29 16:33:11 +0200

    Fix search dropdown
    Needed to switch to angular-material master branch because of
    https://github.com/angular/material/issues/1798
    
    Should switch back to release branch when 0.10 is released (due May 1st)

--- client/app/modules/core/controllers/header-controller.js
+++ client/app/modules/core/controllers/header-controller.js
@@ -1,15 +1,10 @@
 export function HeaderController($scope, $state, $q, Search) {
   this.searchText = '';
-  
+
 
   this.querySearch = (value) => {
-    var promises = Search.byComponent(this.getComponent(), value);
-    var results = [];
-    return $q.all(promises).then(data => {
-      data.map(item => {
-        results = item;
-      });
-      return results.results;
+    return Search.byComponent(this.getComponent(), value).$promise.then((data) => {
+      return data.results;
     });
   };
 
@@ -18,19 +13,19 @@ export function HeaderController($scope, $state, $q, Search) {
       return $state.current.data.component;
     return 'all';
   };
-  
+
   $scope.component = this.getComponent();
-    
+
   $scope.$on('$stateChangeSuccess', () => {
     $scope.component = this.getComponent();
   });
-  
-  
-  this.searchTextChange = (text) => {
+
+
+  this.searchTextChanged = (text) => {
     console.log('Text changed to ' + text);
   };
   this.selectedItemChange = (item) =>  {
     console.log('Item changed to ' + JSON.stringify(item));
   };
-  
+
 }
