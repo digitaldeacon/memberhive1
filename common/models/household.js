b1d3c4e8e0ab2f9adfdf0fce33c45ecf586fdbea
Samuel Mehrbrodt <s.mehrbrodt@gmail.com>
2015-05-12 15:22:44 +0200

    GEM-49 Delete households

--- common/models/household.js
+++ common/models/household.js
@@ -1,3 +1,26 @@
 module.exports = function(Household) {
+  Household.trash = function(householdId, cb) {
+
+    // Need to reset the default scope because of https://github.com/strongloop/loopback/issues/1018
+    var defaultScope = Household.defaultScope;
+    Household.defaultScope = function(){};
+
+    Household.upsert({id:householdId, 'deleted': true}, function(err, obj){
+      cb(null, '');
+    });
+
+    // Restore the default scope
+    Household.defaultScope = defaultScope;
+  };
+  Household.remoteMethod(
+    'trash',
+    {
+      accepts: {
+        arg: 'id',
+        type: 'string',
+        required: true
+      }
+    }
+  );
 
 };
