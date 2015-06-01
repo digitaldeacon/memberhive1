627162f3e06f288453e49865922533368024c45a
Samuel Mehrbrodt <s.mehrbrodt@gmail.com>
2015-06-01 10:46:46 +0200

    Make code more readable

--- common/models/account.js
+++ common/models/account.js
@@ -1,15 +1,18 @@
 var loopback = require('loopback');
+
 module.exports = function(Account) {
   var Role = loopback.Role;
   var RoleMapping = loopback.RoleMapping;
+
   Account.roles = function(msg, cb) {
     Role.getRoles({principalType: RoleMapping.USER, principalId: msg}, function(err, roles) {
-      Role.find({},function(err,allRoles) {
+      Role.find({}, function(err, allRoles) {
         var ret = [];
-        for(i = 0; i < roles.length; i++) {
-          if(typeof roles[i] === "number") {
-            for(j = 0; j < allRoles.length; j++) {
-              if(allRoles[j].id == roles[i]) ret.push(allRoles[j].name);
+        for (var i = 0; i < roles.length; i++) {
+          if (typeof roles[i] === 'number') {
+            for (var j = 0; j < allRoles.length; j++) {
+              if (allRoles[j].id == roles[i])
+                ret.push(allRoles[j].name);
             }
           } else {
             ret.push(roles[i]);
@@ -18,8 +21,7 @@ module.exports = function(Account) {
         cb(null, ret);
       });
     });
-
-  }
+  };
 
   Account.remoteMethod(
     'roles',
