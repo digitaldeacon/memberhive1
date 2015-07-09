6485c2ec1d4dd5d18bf7ab15c7b4646f52046040
Paul Walger <metaxy@walger.name>
2015-07-09 20:38:33 +0200

    first real test

--- e2e/main.spec.js
+++ e2e/main.spec.js
@@ -4,12 +4,13 @@ describe('The Login page', function () {
   var page;
 
   beforeEach(function () {
-    browser.get('http://localhost:9000/index.html');
+    browser.get('http://localhost:9000/#/login');
   });
 
  
   it('Login Page', function () {
-    expect(true);
+    var list = element.all(by.css('.form-title'));
+    expect(list.count()).toBe(1);
   });
 
 });
