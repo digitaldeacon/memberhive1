7d5780595b5ecee3b0bea033d78a6db8833e1894
Paul Walger <metaxy@walger.name>
2015-08-06 22:29:07 +0200

    production config

--- server/config.production.js
+++ server/config.production.js
@@ -4,6 +4,6 @@ module.exports = {
   host: process.env.DB_PORT_27017_TCP_ADDR,
   port: process.env.DB_PORT_27017_TCP_PORT,
   database: process.env.MH_DB_NAME,
-  username: "memberhive",
+  username: process.env.MH_DB_USER,
   password: process.env.MH_DB_PASSWORD
 }; 
