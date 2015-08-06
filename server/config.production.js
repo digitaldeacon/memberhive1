b72f1c89fa9a8399212dbc17b1a318e2a17e5319
Paul Walger <metaxy@walger.name>
2015-08-06 22:45:18 +0200

    fix config for production

--- server/config.production.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name : "db",
-  connector: "mongodb",
-  host: process.env.DB_PORT_27017_TCP_ADDR,
-  port: process.env.DB_PORT_27017_TCP_PORT,
-  database: process.env.MH_DB_NAME,
-  username: process.env.MH_DB_USER,
-  password: process.env.MH_DB_PASSWORD
-}; 
