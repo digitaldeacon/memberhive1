51ef4d9a309d4e990b6c3cc6f0df9ee79172e0b8
Paul Walger <metaxy@walger.name>
2015-08-07 21:09:39 +0200

    fix log

--- common/models/account.js
+++ common/models/account.js
@@ -6,8 +6,8 @@ module.exports = function(Account) {
 
   Account.roles = function(msg, cb) {
     console.log("get roles");
-    console.log(model.getDataSource()),
-    console.log(model.getDataSource().connector);
+    console.log(Account.getDataSource()),
+    console.log(Account.getDataSource().connector);
     Role.getRoles({principalType: RoleMapping.USER, principalId: msg}, function(err, roles) {
       Role.find({}, function(err, allRoles) {
         var ret = [];
