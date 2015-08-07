daca9dbca3cb8ff084722cfdf74925e124db0f78
Paul Walger <metaxy@walger.name>
2015-08-07 21:12:29 +0200

    remove log

--- common/models/account.js
+++ common/models/account.js
@@ -5,9 +5,6 @@ module.exports = function(Account) {
   var RoleMapping = loopback.RoleMapping;
 
   Account.roles = function(msg, cb) {
-    console.log("get roles");
-    console.log(Account.getDataSource()),
-    console.log(Account.getDataSource().connector);
     Role.getRoles({principalType: RoleMapping.USER, principalId: msg}, function(err, roles) {
       Role.find({}, function(err, allRoles) {
         var ret = [];
