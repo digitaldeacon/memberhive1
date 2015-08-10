044ccacad6cb9542978bae37b8b4ab06471352cf
Paul Walger <metaxy@walger.name>
2015-08-10 18:27:11 +0200

    fix avatar create

--- common/models/avatar.js
+++ common/models/avatar.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var fs = require('fs');
 var bunyan = require('bunyan');
+var lwip = require('lwip');
 var log = bunyan.createLogger({name: 'gem.avatar'});
 
 module.exports = function(Avatar) {
@@ -18,14 +19,14 @@ module.exports = function(Avatar) {
    * Create the container (=folder named by userId) if it doesn't exist
    */
   Avatar.beforeRemote('upload', function(ctx, res, next) {
-    console.log("before remote upload");
+    console.log("avatar before remote upload");
     var personId = ctx.req.params.container;
     Avatar.getContainer(personId, function(err, container){
       if (err && err.code == 'ENOENT') { // Container doesn't exist
         Avatar.createContainer({name: personId}, function(err, container) { next();});
         console.log("container created");
       } else {
-         next();
+        next();
       }
     });
    
@@ -35,45 +36,35 @@ module.exports = function(Avatar) {
    * Check input file and create thumbnails
    */
   Avatar.afterRemote('upload', function(ctx, res, next) {
-    console.log("after remote");
+    console.log("avatar after remote");
     var inputfile = res.result.files.file[0];
-
-    var folder = path.join(self.uploadPath, inputfile.container);
-    var originalfile = path.join(folder, inputfile.name);
-    var copyfile = path.join(folder, 'copy.jpg');
-
-    // Weird bug: When trying to create thumbnails from `originalfile`, thumbnails will be kaputt.
-    // When operating on `copyfile`, it works, whyever.
-    // https://github.com/strongloop/loopback-component-storage/issues/56
-    // https://github.com/strongloop/loopback-component-storage/issues/58
-    fs.createReadStream(originalfile).pipe(fs.createWriteStream(copyfile));
-
+    var folderPath = path.join(self.uploadPath, inputfile.container);
+    var filePath = path.join(folderPath, inputfile.name);
     if (inputfile.type != 'image/png' && inputfile.type != 'image/jpg' && inputfile.type != 'image/jpeg') {
-      fs.unlinkSync(originalfile);
-      fs.unlinkSync(copyfile);
+      fs.unlinkSync(filePath);
       next(new Error('Wrong file type. Only jpg and png are supported.'));
       return;
     }
-
-    Object.keys(self.thumbSizes).forEach(function(size) {
-      lwip.open(copyfile, function(err, image) {
-        if (err) {
-          log.error(err);
-          next(new Error('Could not read image file.'));
-          return;
-        }
+    //hacky callbacks
+    // FIXME: use a async library or q
+    this.createThumb(filePath, folderPath, 'xs',
+      this.createThumb(filePath, folderPath, 's',
+        this.createThumb(filePath, folderPath, 'm',
+          this.createThumb(filePath, folderPath, 'l', function(err) { next();})
+        )
+      )
+    )();
+  });
+  
+  this.createThumb = function (filePath, folder, size, cb) {
+    return function(err) {
+      lwip.open(filePath, function(err, image) {
         image.batch()
           .resize(self.thumbSizes[size])
-          .writeFile(path.join(folder, size+".jpg"), function(err) {
-            if (err) {
-              log.error(err);
-              next(new Error('Could not create image thumbnails.'));
-            }
-          });
+          .writeFile(path.join(folder, size+".jpg"), cb);
       });
-    });
-    next();
-  });
-
+    };
+  };
+    
 
 };
