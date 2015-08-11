dd22b7555e3104e13689e9e7eed5de575dda9a04
Paul Walger <metaxy@walger.name>
2015-08-11 10:55:17 +0200

    fix datasources for production

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,7 +1,12 @@
 module.exports = {
-  db : {
-    name: "db",
-    connector: "memory"
+  db: {
+    name : "db",
+    connector: "mongodb",
+    host: process.env.DB_PORT_27017_TCP_ADDR,
+    port: process.env.DB_PORT_27017_TCP_PORT,
+    database: process.env.MH_DB_NAME,
+    username: process.env.MH_DB_USER,
+    password: process.env.MH_DB_PASSWORD,
   },
   "uploads.avatar": {
     name: "uploads.avatar",
