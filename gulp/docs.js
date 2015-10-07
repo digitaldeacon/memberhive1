d2a53cf2276d555edcb64e3a5bb42700660e398c
Paul Walger <metaxy@walger.name>
2015-10-07 15:08:43 +0200

    better docs

--- gulp/docs.js
+++ gulp/docs.js
@@ -4,8 +4,12 @@ var gulp = require('gulp');
 var gulpDocs = require('gulp-ngdocs');
 module.exports = function(options) {
   gulp.task('ngdocs', [], function () {
+    var options = {
+      html5Mode: false,
+      title: "Memberhive Client docs",
+    };
     return gulp.src('client/app/scripts/lb-services.js')
-      .pipe(gulpDocs.process())
+      .pipe(gulpDocs.process(options))
       .pipe(gulp.dest('./dist/docs'));
   });
 };
