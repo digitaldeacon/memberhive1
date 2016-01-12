09e53cf607ac0bc5ff100b21ecbc2eb8c7737b84
Paul Walger <metaxy@walger.name>
2016-01-12 20:45:51 +0100

    new deploy method

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-rsync -az --force --delete --progress -e ssh ./dist/ memberhive@148.251.133.116:hives/data/dist/
-git push memberhive@148.251.133.116:hives/data/git :master # first delete master branch
+git add dist/ -f
+git commit -am "deploy"
 git push memberhive@148.251.133.116:hives/data/git master # now push everything
