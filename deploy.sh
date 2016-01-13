3c4db41d1ed7a754882e11c35fb171c3aa314603
Paul Walger <metaxy@walger.name>
2016-01-13 08:48:47 +0100

    build only master

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 git add dist/ -f
 git commit -am "deploy"
+git push memberhive@148.251.133.116:hives/data/git :master # delete master
 git push memberhive@148.251.133.116:hives/data/git master # now push everything
