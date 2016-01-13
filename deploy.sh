02d707abd82548bc99c0013792a717f222756ce9
Paul Walger <metaxy@walger.name>
2016-01-13 09:55:58 +0100

    add stuff

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+git config --global user.email "travis@memberhive.com"
+git config --global user.name "Travis Buidl"
 git add dist/ -f
 git commit -am "deploy"
 git push memberhive@148.251.133.116:hives/data/git :master # delete master
