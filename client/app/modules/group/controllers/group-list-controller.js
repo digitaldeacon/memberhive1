ff9681633418c442239e081b5524da728e4ae940
Paul Walger <metaxy@walger.name>
2016-01-20 16:09:58 +0100

    delete groups

--- client/app/modules/group/controllers/group-list-controller.js
+++ client/app/modules/group/controllers/group-list-controller.js
@@ -1,6 +1,15 @@
 export function GroupListController(
   GroupService,
-  resolveGroups
+  resolveGroups,
+  Shout,
+  Group
 ) {"ngInject";
   this.groups = resolveGroups;
+
+  this.deleteGroup = (group) => {
+    Group.deleteById({id: group.id}).then((data) => {
+      Shout.success("Group deleted");
+      this.groups = GroupService.all();
+    });
+  };
 }
