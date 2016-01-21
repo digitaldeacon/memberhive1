1a9a73b598dfacfdaca483a876194ee7d0a8457a
Paul Walger <metaxy@walger.name>
2016-01-21 12:21:48 +0100

    delete persons from groups

--- client/app/modules/group/controllers/group-view-controller.js
+++ client/app/modules/group/controllers/group-view-controller.js
@@ -2,10 +2,17 @@ export function GroupViewController(
   GroupService, 
   resolveGroup,
   resolvePersons,
+  Group,
+  Person,
   Shout,
   $state
 ) {"ngInject";
   this.group = resolveGroup;
   this.persons = resolvePersons;
  
+  this.deletePerson = (person) => {
+    Person.groups.unlink({id: person.id, fk: this.group.id}).$promise
+      .then((d) => Shout.success(person.firstName + " deleted"));
+      this.persons = Group.persons({id: this.group.id}).$promise;
+  };
 }
