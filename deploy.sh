5eefbf3bebee4ca51e357a4f4c588b66ee4ba6d4
Paul Walger <metaxy@walger.name>
2016-01-21 11:13:53 +0100

    better travis

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 git config --global user.email "travis@memberhive.com"
 git config --global user.name "Travis Build"
-git add dist/ -f
+ls dist/
+git add dist/* -f
 git commit -am "build: $TRAVIS_BUILD_NUMBER of $TRAVIS_COMMIT"
 git push memberhive@148.251.133.116:hives/data/git $(git log -1 --format="%H"):master -f # now push everything
