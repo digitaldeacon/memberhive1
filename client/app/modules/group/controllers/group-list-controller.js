ff2409fc47d8be4f5a37930d3b4ca782dcbc1274
Paul Walger <metaxy@walger.name>
2016-01-21 12:07:01 +0100

    fix group delete

--- client/app/modules/group/controllers/group-list-controller.js
+++ client/app/modules/group/controllers/group-list-controller.js
@@ -7,7 +7,7 @@ export function GroupListController(
   this.groups = resolveGroups;
 
   this.deleteGroup = (group) => {
-    Group.deleteById({id: group.id}).then((data) => {
+    Group.deleteById({id: group.id}).$promise.then((data) => {
       Shout.success("Group deleted");
       this.groups = GroupService.all();
     });
