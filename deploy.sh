f9d3e47ff7db5091746911171b70f598664d10dc
Paul Walger <metaxy@walger.name>
2016-02-17 20:59:04 +0100

    try to add a npm build to the resulting deploy

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,8 @@ git config --global user.email "travis@memberhive.com"
 git config --global user.name "Travis Build"
 ls dist/
 git add dist/* -f
+git add bower_components/* -f
+git add node_modules/* -f
+
 git commit -am "build: $TRAVIS_BUILD_NUMBER of $TRAVIS_COMMIT"
 git push memberhive@148.251.133.116:hives/data/git $(git log -1 --format="%H"):master -f # now push everything
