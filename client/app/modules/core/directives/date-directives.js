b5ab1edbdf487086c480869b12c8cf1e389c845d
Paul Walger <metaxy@walger.name>
2016-02-24 15:20:22 +0100

    remove log

--- client/app/modules/core/directives/date-directives.js
+++ client/app/modules/core/directives/date-directives.js
@@ -3,15 +3,12 @@ export function utcDate() {
     require: 'ngModel',
     link: function(scope, elem, attrs, ngModel) {
       var toView = function(val) {
-        console.log("to view", val);
         return val;
       };
 
       var toModel = function(val) {
-        console.log("to model", val);
         var offset = moment(val).utcOffset();
         var date = new Date(moment(val).add(offset, 'm'));
-        console.log("to model return", date);
         return date;
       };
 
@@ -34,7 +31,7 @@ export function mhDateInput() {"ngInject";
         $scope.ngModel = new Date();
       } else {
         if(!angular.isDate($scope.ngModel)) {
-          $scope.ngModel = moment($scope.ngModel).toDate();
+          $scope.ngModel = new Date($scope.ngModel);
         }
       }
     }
@@ -50,16 +47,13 @@ export function mhUtcDateInput() {"ngInject";
       placeholder: '@'
     },
     controller: ($scope) => {"ngInject";
-      console.log("utc date $scope.ngModel", $scope.ngModel);
       if(!$scope.ngModel) {
         $scope.ngModel = new Date();
       } else {
         if(!angular.isDate($scope.ngModel)) {
           $scope.ngModel = new Date($scope.ngModel);
-          //$scope.ngModel = moment.utc($scope.ngModel).toDate();
         }
       }
-      console.log("utc date $scope.ngModel after", $scope.ngModel);
     }
   };
 }
