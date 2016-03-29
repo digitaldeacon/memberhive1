22a75a7378e9e369fba4fce9227c4156b1c4cd3e
Paul Walger <metaxy@walger.name>
2016-03-29 10:44:41 +0200

    build slc

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 git config --global user.email "travis@memberhive.com"
 git config --global user.name "Travis Build"
+git fetch origin
 ls dist/
 git add dist/* -f
-slc build -g  --onto master
+slc build -g --onto master
 git commit -am "build: $TRAVIS_BUILD_NUMBER of $TRAVIS_COMMIT"
 git push memberhive@148.251.133.116:hives/data/git $(git log -1 --format="%H"):master -f # now push everything
