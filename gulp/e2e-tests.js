f8a072ea635dd37f1cfebbb95f1d447b57448d20
Paul Walger <metaxy@walger.name>
2016-04-07 15:27:09 +0200

    sauce tests

--- gulp/e2e-tests.js
+++ gulp/e2e-tests.js
@@ -5,7 +5,7 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
 var browserSync = require('browser-sync');
-var gutil = require('gulp-util');
+var sauceConnectLauncher = require('sauce-connect-launcher');
 module.exports = function(options) {
   // Downloads the selenium webdriver
   gulp.task('webdriver-update', $.protractor.webdriver_update);
@@ -31,4 +31,36 @@ module.exports = function(options) {
   gulp.task('protractor', ['protractor:src']);
   gulp.task('protractor:src', ['serve:e2e', 'webdriver-update'], runProtractor);
   gulp.task('protractor:dist', ['serve:e2e-dist', 'webdriver-update'], runProtractor);
+  
+  // connects to saucelabs server and runs protractor e2e tests against saucelabs server
+  gulp.task('protractor:src:sauce', ['serve:e2e'],, function() {
+    sauceConnectLauncher({
+      username: process.env.SAUCE_USERNAME,
+      accessKey:  process.env.SAUCE_ACCESS_KEY,
+      // logger: console.log
+    }, function (err, sauceConnectProcess) {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+      console.log("Sauce Connect ready");
+
+       gulp.src(options.e2e + '/**/*.js')
+        .pipe($.protractor.protractor({
+          configFile: 'protractor.travis.conf.js'
+        }))
+        .on('error', function(e) {
+          sauceConnectProcess.close(function () {
+            console.log("Closed Sauce Connect process");
+          });
+          throw e;
+        })
+        .on('end', function(e) {
+          sauceConnectProcess.close(function () {
+            console.log("Closed Sauce Connect process");
+          });
+        });
+
+    });
+  });
 };
--- protractor.travis.conf.js
+++ protractor.travis.conf.js
@@ -2,13 +2,22 @@
 exports.config = {
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
-  
+  capabilities: {
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+    'build': process.env.TRAVIS_BUILD_NUMBER
+  },
   multiCapabilities: [
     {
       browserName : 'firefox'
     },
     {
        browserName: 'chrome'
+    },
+    {
+        browserName: 'internet explorer',
+    },
+    {
+        browserName: 'Safari',
     }
   ],
 
