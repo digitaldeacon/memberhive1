4e7b8f766ed0836440911676baa077aca07280f0
Paul Walger <metaxy@walger.name>
2016-04-07 16:35:50 +0200

    fix

--- protractor.travis.conf.js
+++ protractor.travis.conf.js
@@ -3,7 +3,7 @@ exports.config = {
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
   capabilities: {
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER
   },
   multiCapabilities: [
