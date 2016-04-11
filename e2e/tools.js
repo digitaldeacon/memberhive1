2b3a0888d7677d367bf916347a836c4893ee0c6d
Paul Walger <metaxy@walger.name>
2016-04-11 19:15:00 +0200

    fix this

--- e2e/tools.js
+++ e2e/tools.js
@@ -6,7 +6,11 @@ exports.noLogErrors = () => {
       let severWarnings = false;
 
       for(i; i < browserLog.length; i++){
-        if(browserLog[i].level.name === 'SEVERE' && !browserLog[i].message.includes("WebSocket connection to")){
+        if(browserLog[i].level.name === 'SEVERE' 
+          && !browserLog[i].message.includes("WebSocket connection to")
+          && !browserLog[i].message.includes("http://127.0.0.1:3994/api/Accounts 0:0 Failed to load resource: the server responded with a status of 401 (Unauthorized)")
+          && !browserLog[i].message.includes("http://127.0.0.1:3994/api/Accounts/roles 0:0 Failed to load resource: the server responded with a status of 401 (Unauthorized)")
+        ){
           console.log('(Possibly exception) \n' + browserLog[i].message);
           severWarnings = true;
         }
