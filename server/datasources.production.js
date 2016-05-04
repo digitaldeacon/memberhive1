776a9b8371cfa8e7b5500006e9f6c7da7096a892
Paul Walger <metaxy@walger.name>
2016-05-04 13:42:50 +0200

    update config for docker compose

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -2,8 +2,8 @@ module.exports = {
   db: {
     name : "db",
     connector: "mongodb",
-    host: process.env.DB_PORT_27017_TCP_ADDR,
-    port: process.env.DB_PORT_27017_TCP_PORT,
+    host: 'db',
+    port: '27017',
     database: process.env.MH_DB_NAME,
     username: process.env.MH_DB_USER,
     password: process.env.MH_DB_PASSWORD,
