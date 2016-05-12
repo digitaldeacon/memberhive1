1525bf51e39052e928a185191d017c289a077c22
Paul Walger <metaxy@walger.name>
2016-05-12 14:39:56 +0200

    workin next events

--- client/app/modules/event/widgets/next-events/next-events.js
+++ client/app/modules/event/widgets/next-events/next-events.js
@@ -2,9 +2,8 @@ var mhWidgetNextEvents = function () {
   return {
     templateUrl: 'app/modules/event/widgets/next-events/view.html',
     restrict: 'E',
-    controllerAs : 'ctrl',
     controller: ($scope, EventService, EventStatusOptions) => {"ngInject";
-      $scope.nextEvents = EventService.future({limit: 10});
+      EventService.future({limit: 10}).then(d => $scope.nextEvents = d);
       $scope.countStatus = EventService.countStatus;
       $scope.statusOptions = EventStatusOptions;
 
