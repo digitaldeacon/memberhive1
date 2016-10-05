34ba46cac91ac90d886b6d2d85cf4d9de431a28e
Paul Walger <metaxy@walger.name>
2016-10-05 12:07:54 +0200

    fix see https://github.com/strongloop/loopback/issues/1495 https://github.com/strongloop/loopback-datasource-juggler/issues/657

--- common/models/note.js
+++ common/models/note.js
@@ -13,11 +13,11 @@ module.exports = function(Note) {
 
   //return only the results, which belong to the user
   Note.observe('access', function limitToUser(ctx, next) {
-    var userId = Note.app.loopback.getCurrentContext().get('accessToken').userId;
+    //var userId = Note.app.loopback.getCurrentContext().get('accessToken').userId;
     //ctx = utils.whereAddAnd(ctx, {"ownerId": userId.toString()});
     next();
   });
-  
+
   Note.createRemotes();
 
 };
