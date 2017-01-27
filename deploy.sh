401fb0740387f44de3c11216da8703a94f7358d4
Paul Walger <metaxy@walger.name>
2017-01-27 13:22:15 +0100

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ ls dist/
 git add dist/* -f
 #sl-build -g --onto master
 git commit -am "build: $TRAVIS_BUILD_NUMBER of $TRAVIS_COMMIT"
-git push memberhive@148.251.133.116:hives/data/git $(git log -1 --format="%H"):master -f # now push everything
+git push memberhive@memberhive.com:hives/data/git $(git log -1 --format="%H"):master -f # now push everything
