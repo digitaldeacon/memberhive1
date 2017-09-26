91c58087a1306395b2e131940ea6a558fc1069f3
Paul Walger <metaxy@walger.name>
2017-09-26 10:10:24 +0200

    fix versions

--- test.sh
+++ test.sh
@@ -1,15 +1,16 @@
 set -e
 function server() {
     local port="${1:-3000}"
-    sleep 1 && open "http://localhost:${port}/" &
+    sleep 1 && chromium-browser "http://localhost:3000" &
     # Set the default Content-Type to `text/plain` instead of `application/octet-stream`
     # And serve everything as UTF-8 (although not technically correct, this doesnâ<80><99>t break anything for binary    files)
     python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] =     "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port"
 }
 rm dist -rf
 rm node_modules -rf
+rm bower_components -rf
 npm install
+bower install
 gulp build
 cd dist/
 server
-chromium-browser https://localhost:3000
